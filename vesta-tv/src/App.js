fa0a75d551f5add35ff0ef50ffd8f0d0aa6020d5
Viktoryia <Viktoryia_Kakoshanka@epam.com>
2020-01-16 18:39:20 +0300

    +

--- vesta-tv/src/App.js
+++ vesta-tv/src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/Tamplates/index.ntml"
           target="_blank"
           rel="noopener noreferrer"
         >
